98bec6289d90720a0bba57831d2d02434f86fbd4
Verteletsky <verteletsky@bitrix24.ru>
2017-03-18 19:30:15 +0300

    new class and refactoring method

---
+++ Snake/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Snake
+{
+	public class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+
+		public void draw()
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);
+		}
+	}
+}
